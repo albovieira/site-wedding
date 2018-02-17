57c45ee161c4e4eb9c6f2722869b58083e86f9e1
Albo Vieira <albovieira@gmail.com>
2018-02-16 23:46:51 -0200

    fix

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <b-jumbotron style="margin-bottom:0" class="cursiveFont bg text-center" header="Camila & Albo">
+      <b-jumbotron class="cursiveFont bg text-center" header="Camila & Albo">
         <template slot="lead">
           <div>
             <icon name="heart"></icon>
@@ -45,12 +45,13 @@ export default {
   methods: {
     timer() {
       const eventTime = moment('2018-09-07 16:30');
-      const currentTime = new Date();
+      const currentTime = moment();
       const diffDays = eventTime.diff(currentTime, 'days');
       const diffTime = eventTime.diff(currentTime, 'milliseconds');
 
       // const diffTime = eventTime - currentTime;
       let duration = moment.duration(diffTime, 'milliseconds');
+      console.log(duration);
       const interval = 1000;
 
       const self = this;
@@ -58,9 +59,16 @@ export default {
         duration = moment.duration(duration - interval, 'milliseconds');
         self.countdown = {
           days: `${diffDays}`,
-          hours: duration.hours(),
-          minutes: duration.minutes(),
-          seconds: duration.seconds()
+          hours:
+            duration.hours() < 10 ? `0${duration.hours()}` : duration.hours(),
+          minutes:
+            duration.minutes() < 10
+              ? `0${duration.minutes()}`
+              : duration.minutes(),
+          seconds:
+            duration.seconds() < 10
+              ? `0${duration.seconds()}`
+              : duration.seconds()
         };
       }, interval);
     }
@@ -71,27 +79,33 @@ export default {
 <style lang="scss" scoped>
 @import '~assets/scss/index.scss';
 
+.jumbotron {
+  padding-top: 1rem;
+  margin-bottom: 0;
+}
 .bg {
   background: url('/static/imgs/banner.png') no-repeat;
-  background-position: center 12rem;
+  background-position: center 10rem;
   background-color: #cfd9e2;
-  height: 26rem;
-  background-size: 35rem;
+  height: 28rem;
+  background-size: 19rem;
 }
 
 .container-countdown {
-  background: #71b8f7;
+  background: #a2bfd8;
   color: white;
   text-align: center;
+  padding: 0 8rem;
+  text-shadow: 1px 1px 2px black;
 }
 .countdown {
   line-height: 1.3;
   padding: 1rem;
   strong {
-    font-size: 4rem;
+    font-size: 2rem;
   }
   .time-description {
-    font-size: 2rem;
+    font-size: 1rem;
   }
 }
 </style>
