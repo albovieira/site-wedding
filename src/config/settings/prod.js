b627cf990f2f353d4de4e9328ae9eaa12385bdb0
Albo Vieira <albovieira@gmail.com>
2018-06-24 13:47:07 -0300

    Adding files.

--- src/config/settings/prod.js
+++ src/config/settings/prod.js
@@ -1,8 +1,20 @@
 export default {
   enviroment: 'dev',
+  weddingAPI: {
+    url: 'https://nme56dww83.execute-api.us-east-1.amazonaws.com/dev/',
+    endpoints: {
+      guests: 'guests',
+      wall_messages: 'wall-messages',
+      wished_musics: 'wished-musics'
+    }
+  },
   uber: {
     endpoint: 'https://api.uber.com/v1.2/estimates/price',
-    token: 'Token EiSC1gcBMNpsmN4PC5GlEkQ6WjQuU0amxa7HLRC_'
+    token: 'EiSC1gcBMNpsmN4PC5GlEkQ6WjQuU0amxa7HLRC_'
+  },
+  spotify: {
+    token:
+      'BQD3-GI_lXZStXWgBBidWJdxPXOWOaQWzw41yzGnMLKTedTQlum5X5dmgCgKAKtMmbFyCrBDvxSQwGMc7ZpaVsMdBy_yMaBp99oKXk9dLPMOtFmO3VZwr_wY3X28uBaYtBTh4fwt4qtgYyvvSqVaAHJZQE4TCeRMlVU_283oqOSsfqrdHzUYsw'
   },
   google: {
     maps: 'https://maps.googleapis.com/maps/api/',
