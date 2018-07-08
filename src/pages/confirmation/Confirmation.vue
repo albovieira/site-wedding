c0abc6bee15e99df2575f272d573d16a037974f2
Albo Vieira <albovieira@gmail.com>
2018-07-07 21:07:41 -0300

    fix

--- src/pages/confirmation/Confirmation.vue
+++ src/pages/confirmation/Confirmation.vue
@@ -180,6 +180,7 @@ export default {
         evt.preventDefault();
         if (!this.email) {
           swal('Preencha seu email', 'Digite seu email', 'error');
+          return;
         }
 
         const payload = {
