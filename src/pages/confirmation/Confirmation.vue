feff9a36b5aa7a8a3b121e757ea0dc0299854729
Albo Vieira <albovieira@gmail.com>
2018-07-27 00:34:27 -0300

    fix

--- src/pages/confirmation/Confirmation.vue
+++ src/pages/confirmation/Confirmation.vue
@@ -7,7 +7,7 @@
         <b-col md="4" sm="12">
             <b-card class="post-it post-it-tip" text-variant="white" title="Traje - Social">
               <p class="card-text">
-                Conferir <a href="https://br.pinterest.com/mimicaelias/casamento-no-campo/" target="_blank"><strong>DICAS</strong></a>
+                Conferir algumas <a href="https://br.pinterest.com/mimicaelias/casamento-no-campo/" target="_blank"><strong>inspirações</strong></a>
               </p>
 
               <div class="container-img">
